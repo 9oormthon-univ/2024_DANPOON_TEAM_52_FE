4837127504cce2943cfd55925c7fed21d5427808
c9u11 <tjrdud6412@naver.com>
2024-11-18 00:38:56 +0900

    ✨ Router 기본 설정
    - 기본 Constants 설정
    - Root로 접속 시 Home으로 이동

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {
+  ROUTES_BASENAME,
+  ROUTES_PATH_ROOT,
+  ROUTES_PATH_HOME,
+} from "./constants/routes";
+
+export default function App() {
   return (
-    <div>
-      APP JS
-    </div>
+    <BrowserRouter basename={ROUTES_BASENAME}>
+      <Routes>
+        <Route
+          path={ROUTES_PATH_ROOT}
+          element={<Navigate to={ROUTES_PATH_HOME} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
