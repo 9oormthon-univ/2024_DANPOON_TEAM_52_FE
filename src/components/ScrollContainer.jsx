0c5d8ca62db7e693e5e402ccd36a5be3031eaca8
c9u11 <tjrdud6412@naver.com>
2024-11-19 02:08:35 +0900

    💄 Scroll Container Component

---
+++ src/components/ScrollContainer.jsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components"
+
+const ScrollContainer = styled.div`
+  overflow: auto;
+  padding-right: 10px;
+  &::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #2f2f2f;
+    border-radius: 10px;
+  }
+`
+
+export default ScrollContainer
