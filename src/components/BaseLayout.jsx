84e10e1038f64018c9a2069752b702ad79e8bcbb
c9u11 <tjrdud6412@naver.com>
2024-11-19 02:48:32 +0900

    💄 BaseLayout overflow 설정
    - overflow를 설정하지않으면 원하는 크기보다 커질 수 있음

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -17,7 +17,8 @@ const ChildrenContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
+  flex: 1;
+  overflow: hidden;
 `
 
 const NavLink = styled(Link)`
