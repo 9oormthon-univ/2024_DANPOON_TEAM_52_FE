205195465603e8d015601f02b4d31bbc12a8cfae
c9u11 <tjrdud6412@naver.com>
2024-11-23 18:55:57 +0900

    💄 Search Page Design

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,114 @@
+import { Flex } from "antd"
 import BaseLayout from "../../components/BaseLayout"
+import Goals from "../../components/Goals"
+import {
+  Container,
+  Description,
+  Header,
+  IconButton,
+  TitleButton,
+  CategoryContainer,
+  Category,
+} from "./styled"
+import { ReactComponent as SortSVG } from "../../svgs/Sort.svg"
+import { ReactComponent as DownSVG } from "../../svgs/Down.svg"
 
 export default function Search() {
-  return <BaseLayout>search</BaseLayout>
+  const loading = false
+  const list = [
+    {
+      id: 1,
+      icon: "️🗓️",
+      title: "구름톤 회의",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 2,
+      icon: "📝",
+      title: "구름톤 회의록 작성",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 3,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 4,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 5,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 6,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 7,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 7,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+    {
+      id: 7,
+      icon: "📞",
+      title: "구름톤 회의록 검토",
+      description: "2024.01.03",
+      label: "일정",
+    },
+  ]
+  const categories = [
+    { label: "전체", value: "all" },
+    { label: "🏆 자격·어학·수상", value: "schedule" },
+    { label: "🏫 경험·활동·교육", value: "goal" },
+    { label: "🪪 경력", value: "history" },
+    { label: "🎸 기타", value: "history" },
+  ]
+  return (
+    <BaseLayout>
+      <Container>
+        <Header>
+          <Flex vertical gap={12}>
+            <TitleButton>
+              전체 목표 탐색
+              <DownSVG />
+            </TitleButton>
+            <Description>
+              다른 사람들의 목표와 이력을 탐색할 수 있어요
+            </Description>
+          </Flex>
+          <IconButton>
+            <SortSVG />
+          </IconButton>
+        </Header>
+        <CategoryContainer>
+          <Category options={categories} optionType="button" />
+        </CategoryContainer>
+        <Goals goals={list} loading={loading} option={{ labelHidden: true }} />
+      </Container>
+    </BaseLayout>
+  )
 }
---
+++ src/pages/Search/styled.jsx
@@ -0,0 +1,90 @@
+import styled from "styled-components"
+import { Radio } from "antd"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  padding: 20px 33px 0;
+  gap: 30px;
+`
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+export const TitleButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+  background: none;
+  border: none;
+  outline: none;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+`
+
+export const Description = styled.span`
+  color: #c3c3c3;
+  font-size: 13px;
+  font-weight: 500;
+`
+
+export const IconButton = styled.button`
+  background-color: #222;
+  border: none;
+  border-radius: 50%;
+  padding: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+`
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  transform: translateX(-30px);
+  padding: 0 30px;
+  gap: 10px;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+  }
+  &::-webkit-scrollbar-thumb {
+    display: none;
+  }
+`
+
+export const Category = styled(Radio.Group)`
+  &&& {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+  }
+  & .ant-radio-button-wrapper {
+    background-color: #244240;
+    border-radius: 5px;
+    border: none;
+    color: white !important;
+    white-space: nowrap;
+  }
+
+  & .ant-radio-button-wrapper::before {
+    display: none;
+  }
+  & .ant-radio-button-wrapper-checked {
+    background-color: #43b6ae;
+    color: black !important;
+  }
+`
