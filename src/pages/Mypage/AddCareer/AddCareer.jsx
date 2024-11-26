a5970259d1568d181bb5654dd155818ac3ce3faa
kwon <dohun_dev@naver.com>
2024-11-26 18:36:17 +0900

    ✨이력 추가 api연결(데이터구조 통일)

--- src/pages/Mypage/AddCareer/AddCareer.jsx
+++ src/pages/Mypage/AddCareer/AddCareer.jsx
@@ -12,7 +12,7 @@ import { DatePicker } from "antd"
 import { useDateRange } from "../../../hooks/useDateRange"
 import StyledSwitch from "../../../components/Switch"
 import { Text } from "../../../components/Typo"
-
+import { reqPostResume } from "../../../apis/user"
 const DatePickerWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -48,20 +48,51 @@ const AddCareerPage = ({ setIsModalOpen, onAddCareer }) => {
   const [careerName, setCareerName] = useState("")
   const [detail, setDetail] = useState("")
   const [selectedCategory, setSelectedCategory] = useState({
-    id: null,
-    name: "이력 카테고리를 선택해주세요",
+    id: "",
+    name: "",
   })
 
   const { startDate, setStartDate, endDate, setEndDate } = useDateRange()
   const handleAddCareer = (newCareer) => {
-    setItems((prevItems) => [...prevItems, newCareer]);
-  };
+    setItems((prevItems) => [...prevItems, newCareer])
+  }
   const toggleDropdown = () => {
     setIsOpen(!isOpen)
   }
 
   const handleCategoryClick = (category) => {
-    setSelectedCategory(category)
+    switch (category.id) {
+      case 1:
+        setSelectedCategory({
+          id: "1",
+          name: "ACADEMY"
+        })
+        break
+      case 2:
+        setSelectedCategory({
+          id: "2",
+          name: "CAREER"
+        })
+        break
+      case 3:
+        setSelectedCategory({
+          id: "3",
+          name: "QUALIFICATION"
+        })
+        break
+      case 4:
+        setSelectedCategory({
+          id: "4",
+          name: "EXPERIENCE"
+        })
+        break
+      case 5:
+        setSelectedCategory({
+          id: "5",
+          name: "ETC"
+        })
+        break
+    }
     setIsOpen(false)
   }
 
@@ -74,17 +105,17 @@ const AddCareerPage = ({ setIsModalOpen, onAddCareer }) => {
       alert("모든 항목을 입력해주세요.")
       return
     }
-
+    //서버에 등록할 데이터
     const newCareer = {
-      category: selectedCategory.id,
-      itemName: careerName,
-      detail,
-      startDate,
-      endDate,
+      resume_category: selectedCategory.name,
+      title: careerName,
+      content: detail,
+      start_Date: startDate,
+      end_Date: endDate
     }
-
+    console.log(newCareer)
     // 부모 컴포넌트의 업데이트 함수 호출
-    onAddCareer(newCareer)
+    reqPostResume(newCareer);
 
     // 모달 닫기
     setIsModalOpen(false)
@@ -100,11 +131,11 @@ const AddCareerPage = ({ setIsModalOpen, onAddCareer }) => {
               {selectedCategory.id && (
                 <img
                   src={getIconSrc(selectedCategory.id)}
-                  alt={selectedCategory.name}
+                  alt={selectedCategory.name + 123}
                   style={{ width: "20px", height: "20px", marginRight: "10px" }}
                 />
               )}
-              <div style={{ textAlign: "center" }}>{selectedCategory.name}</div>
+              <div style={{ textAlign: "center" }}>{categories[parseInt(selectedCategory.id)-1]?.name}</div>
             </div>
             <S.DropdownArrow isOpen={isOpen}>▼</S.DropdownArrow>
           </S.DropdownHeader>
