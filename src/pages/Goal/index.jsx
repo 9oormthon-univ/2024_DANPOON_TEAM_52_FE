df077c81f087b73c59a64ea52a8ee61b2e0c6369
c9u11 <tjrdud6412@naver.com>
2024-11-28 03:17:36 +0900

    🐛 퀘스트 생성 시 isComplete 없이 생성되는 버그 수정

--- src/pages/Goal/index.jsx
+++ src/pages/Goal/index.jsx
@@ -156,7 +156,7 @@ export default function Quest() {
   const onClickAddQuest = () => {
     setQuestModal({
       open: true,
-      quest: { id: -1, title: "" },
+      quest: { id: -1, title: "", isComplete: false },
     })
   }
   const onCloseQuestModal = () => {
