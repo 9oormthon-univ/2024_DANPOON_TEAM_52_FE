30d488fd6048ede94515055ac46d59969d8fe967
c9u11 <tjrdud6412@naver.com>
2024-11-29 02:18:24 +0900

    ✨ CheckListItem Component 수정 삭제 버튼

--- src/components/CheckListItem.jsx
+++ src/components/CheckListItem.jsx
@@ -1,16 +1,35 @@
 import styled from "styled-components"
-import { Checkbox } from "antd"
-import { useState } from "react"
+import { Checkbox, Flex } from "antd"
+import { useState, useRef } from "react"
 import { motion } from "framer-motion"
 
-const CheckListItem = styled(Checkbox)`
-  position: relative;
+const CheckListItemContainer = styled(motion.div)`
+  display: flex;
   width: 100%;
+  height: fit-content;
+  border-radius: 10px;
+  margin: 0px;
+  cursor: pointer;
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+  & > * {
+    scroll-snap-align: end;
+  }
+  &::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+  }
+  &::-webkit-scrollbar-thumb {
+    display: none;
+  }
+`
+const InnerContainer = styled(Checkbox)`
+  position: relative;
+  min-width: 100%;
   height: fit-content;
   padding: 8px 20px;
   color: #fff;
   background-color: #2c2c2c;
-  border-radius: 10px;
   font-size: 15px;
   font-weight: 500;
   overflow: hidden;
@@ -26,17 +45,47 @@ const CheckListItem = styled(Checkbox)`
     border-color: #8afaf1;
   }
 `
+const EditButton = styled.button`
+  color: #fff;
+  padding: 0 20px;
+  font-size: 13px;
+  font-weight: 600;
+  background-color: #4d4d4d;
+  border: none;
+  outline: none;
+  white-space: nowrap;
+  cursor: pointer;
+`
+const DeleteButton = styled.button`
+  padding: 0 20px;
+  color: #fff;
+  font-size: 13px;
+  font-weight: 600;
+  background-color: #d94242;
+  border: none;
+  outline: none;
+  white-space: nowrap;
+  cursor: pointer;
+`
 
-const usingTokenCheckListItem = ({
+const CheckListItem = ({
   defaultChecked,
   checked,
   onChange,
+  onEdit,
+  onDelete,
+  option,
   ...props
 }) => {
   const [isChecked, setIsChecked] = useState(defaultChecked ?? checked)
+  const listItemRef = useRef(null)
+  const eventHandler = (func) => {
+    listItemRef.current.scrollTo({ behavior: "smooth", left: 0 })
+    func()
+  }
   return (
-    <motion.div layoutId={props.value}>
-      <CheckListItem
+    <CheckListItemContainer layoutId={props.value} ref={listItemRef}>
+      <InnerContainer
         {...props}
         checked={isChecked}
         onChange={(e) => {
@@ -47,8 +96,18 @@ const usingTokenCheckListItem = ({
           background: isChecked ? "#15373D" : "#2c2c2c",
         }}
       />
-    </motion.div>
+      <Flex>
+        {option?.editVisible && (
+          <EditButton onClick={() => eventHandler(onEdit)}>수정</EditButton>
+        )}
+        {option?.deleteVisible && (
+          <DeleteButton onClick={() => eventHandler(onDelete)}>
+            삭제
+          </DeleteButton>
+        )}
+      </Flex>
+    </CheckListItemContainer>
   )
 }
 
-export default usingTokenCheckListItem
+export default CheckListItem
