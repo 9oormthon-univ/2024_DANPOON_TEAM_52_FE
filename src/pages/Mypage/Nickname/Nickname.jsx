416046175c9f7c660f8fe936e7663e2de1c5af8f
kwon <dohun_dev@naver.com>
2024-11-30 11:39:36 +0900

    ✨닉네임 변경창 및 변경사항 저장 구현

--- src/pages/Mypage/Nickname/Nickname.jsx
+++ src/pages/Mypage/Nickname/Nickname.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from "react"
-import { ModalOverlay, ModalContent, ModalInput, ModalButtonGroup, ModalButton} from "../../Calendar/styled"
-
-const NicknamePage = ({ setIsModalOpen }) => {
-    const [nickname, setNickName] = useState("");
+import React, { useEffect, useState } from "react"
+import {
+  ModalOverlay,
+  ModalContent,
+  ModalInput,
+  ModalButtonGroup,
+  ModalButton,
+} from "../../Calendar/styled"
+import userAtom from "../../../store/atoms/user"
+import { useSetRecoilState } from "recoil"
+const NicknamePage = ({ setIsEdit }) => {
+  const [nickname, setNickName] = useState("")
+  const setUserData = useSetRecoilState(userAtom)
+  //변경된 닉네임 아톰에 갱신
+  const handleSaveNickname = () => {
+    setUserData((prevData) => ({
+      ...prevData,
+      nickname: nickname,
+    }))
+    setIsEdit(false)
+  }
   return (
     <ModalOverlay>
       <ModalContent onClick={(e) => e.stopPropagation()}>
@@ -15,8 +31,14 @@ const NicknamePage = ({ setIsModalOpen }) => {
           onChange={(e) => setNickName(e.target.value)}
         />
         <ModalButtonGroup>
-          <ModalButton onClick={() => setIsModalOpen(false)}>취소</ModalButton>
-          <ModalButton onClick={()=>{/*닉네임변경 api*/ setIsModalOpen(false)}}>완료</ModalButton>
+          <ModalButton onClick={() => setIsEdit(false)}>취소</ModalButton>
+          <ModalButton
+            onClick={() => {
+              handleSaveNickname();
+            }}
+          >
+            완료
+          </ModalButton>
         </ModalButtonGroup>
       </ModalContent>
     </ModalOverlay>
