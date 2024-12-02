d02280a725b1ce915ded2c72b69c88dd6e286c64
kwon <dohun_dev@naver.com>
2024-12-03 01:24:12 +0900

    ✨피드백 조회 및 생성 api 작성

--- src/apis/feedback.js
+++ src/apis/feedback.js
@@ -1,7 +1,16 @@
 import apiClient from "./apiClient"
 
 //피드백 생성
-export const reqGetFeedback = async () => {
+export const reqPostFeedback = async () => {
   const response = await apiClient.post("/member/ai")
-  return response.data // 필요한 데이터만 반환
+  return response.data.data // 필요한 데이터만 반환
+}
+
+//피드백 조회
+export const reqGetFeedback = async () => {
+  const response = await apiClient.get("/member/ai")
+  if (response.status === 200) {
+    console.log(response.data)
+  }
+  return response.data.data.ai_feedback // 필요한 데이터만 반환
 }
